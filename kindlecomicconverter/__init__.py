b0bc399c4431fa1386fcff6edc495f5fcafaff73
darodi <4682830+darodi@users.noreply.github.com>
2022-03-07 20:29:54 +0100

    version change

--- kindlecomicconverter/__init__.py
+++ kindlecomicconverter/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '5.5.3-beta-darodi.3'
+__version__ = '5.5.3-beta-darodi.4'
 __license__ = 'ISC'
 __copyright__ = '2012-2019, Ciro Mattia Gonano <ciromattia@gmail.com>, Pawel Jastrzebski <pawelj@iosphe.re>'
 __docformat__ = 'restructuredtext en'
